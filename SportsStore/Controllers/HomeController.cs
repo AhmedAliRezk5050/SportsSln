b5edc14c883cff273320e0a816462d230c6be7db
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-10 02:06:34 +0200

    editions

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -4,31 +4,36 @@ using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
-  public class HomeController : Controller
-  {
-    public int PageSize = 4;
-    private IStoreRepository repository;
-
-    public HomeController(IStoreRepository repo)
+    public class HomeController : Controller
     {
-      repository = repo;
-    }
+        public int PageSize = 4;
+        private IStoreRepository repository;
 
-    public IActionResult Index(string? category, int productPage = 1) =>
-     View(new ProductsListViewModel
-     {
-       Products = repository.Products
-                            .Where(p => category == null || p.Category == category)
-                            .OrderBy(p => p.ProductID)
-                            .Skip((productPage - 1) * PageSize)
-                            .Take(PageSize),
-       PagingInfo = new PagingInfo
-       {
-         TotalItems = repository.Products.Count(),
-         ItemsPerPage = PageSize,
-         CurrentPage = productPage,
-       },
-       CurrentCategory = category,
-     });
-  }
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index(string? category, int productPage = 1)
+        {
+            var products = repository.Products;
+            return View(new ProductsListViewModel
+            {
+                Products = products.Where(p => category == null || p.Category == category)
+                                   .OrderBy(p => p.ProductID)
+                                   .Skip((productPage - 1) * PageSize)
+                                   .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    TotalItems = category == null ?
+                    products.Count()
+                    :
+                    products.Where(p => p.Category == category).Count(),
+                    ItemsPerPage = PageSize,
+                    CurrentPage = productPage,
+                },
+                CurrentCategory = category,
+            });
+        }
+    }
 }

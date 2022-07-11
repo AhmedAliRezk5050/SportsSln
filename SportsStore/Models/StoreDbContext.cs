3a03b10461eee8524b77b883233d2d254aba2069
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-11 04:10:46 +0200

    editions

--- SportsStore/Models/StoreDbContext.cs
+++ SportsStore/Models/StoreDbContext.cs
@@ -4,9 +4,11 @@ namespace SportsStore.Models;
 
 public class StoreDbContext : DbContext
 {
-  public StoreDbContext(DbContextOptions<StoreDbContext> options)
-  : base(options) { }
+    public StoreDbContext(DbContextOptions<StoreDbContext> options)
+    : base(options) { }
 
-  // study
-  public DbSet<Product> Products => Set<Product>();
+    // study
+    public DbSet<Product> Products => Set<Product>();
+
+    public DbSet<Order> Orders => Set<Order>();
 }

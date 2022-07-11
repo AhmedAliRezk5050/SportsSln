41c02b5d8dff43e4b5fc0b13bf87e85cb0a90693
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-11 15:06:42 +0200

    editions

--- SportsStore/Models/IStoreRepository.cs
+++ SportsStore/Models/IStoreRepository.cs
@@ -2,5 +2,11 @@ namespace SportsStore.Models;
 
 public interface IStoreRepository
 {
-  IQueryable<Product> Products { get; }
+    IQueryable<Product> Products { get; }
+
+    void SaveProduct(Product p);
+
+    void CreateProduct(Product p);
+
+    void DeleteProduct(Product p);
 }

8c3e16134c0fcdc797ffbef8e9c2c586804b10a7
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-11 14:04:49 +0200

    Displaying Orders to the Administrator

--- SportsStore/Migrations/20220711111223_alter_orders_add_Shipped.Designer.cs
+++ SportsStore/Migrations/20220711111223_alter_orders_add_Shipped.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsStore.Models;
 
@@ -11,9 +12,10 @@ using SportsStore.Models;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220711111223_alter_orders_add_Shipped")]
+    partial class alter_orders_add_Shipped
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
